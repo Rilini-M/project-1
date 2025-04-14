5a7a4d7825cf3128c7d551cfb5493dee7e187bdc
rilini <rilinimsra1029@gmail.com>
2025-04-14 23:47:46 +0545

    Add common folder code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {Hono} from 'hono';
 import {userRouter} from './routes/user'
 import {blogRouter} from './routes/blog'
@@ -16,3 +17,36 @@ app.route("/api/v1", userRouter)
 app.route("/api/v1/blog", blogRouter)
 
 export default app
+=======
+
+import {z} from 'zod'
+
+export const signupInput = z.object({
+  email:z.string().email(),
+  password:z.string().min(6),
+  name:z.string().optional()
+})
+
+export const signinInput = z.object({
+    email:z.string().email(),
+    password:z.string().min(6)
+  })
+
+  export const createBlogInput = z.object({
+    title:z.string(),
+    content: z.string(),
+  })
+
+  export const updateBlogInput = z.object({
+    title:z.string(),
+    content: z.string(),
+    id: z.number()
+  })
+
+
+
+  export type SignupInput = z.infer<typeof signupInput>
+  export type SigninInput = z.infer<typeof signinInput>
+  export type createBlogInput = z.infer<typeof createBlogInput>
+  export type updatelogInput = z.infer<typeof updateBlogInput>
+>>>>>>> c3855f2 (Add common folder code)
